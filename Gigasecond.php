20de34409fa80b358a402d537626d879bd415dd5
Deivis Cervantes Puello <54411803+dcervantespuello@users.noreply.github.com>
2023-09-01 22:25:55 -0500

    Create Gigasecond.php

---
+++ Gigasecond.php
@@ -0,0 +1,22 @@
+// Instructions
+// Your task is to determine the date and time one gigasecond after a certain date.
+
+// A gigasecond is one thousand million seconds. That is a one with nine zeros after it.
+
+// If you were born on January 24th, 2015 at 22:00 (10:00:00pm), then you would be a gigasecond old on October 2nd, 2046 at 23:46:40 (11:46:40pm).
+
+<?php
+
+declare(strict_types=1);
+
+/**
+* @param DateTimeImmutable $date
+*
+* @return DateTimeImmutable
+*/
+function from(DateTimeImmutable $date): DateTimeImmutable
+{
+    return $date->add(
+        new DateInterval('PT1000000000S')
+    );
+}
